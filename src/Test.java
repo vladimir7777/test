94ab5632698ea9c25039c7fc6b9ca4456509cb3e
unknown <Vladimir@Vladimir-PC.(none)>
2012-10-30 13:19:10 +0200

    Test commit 03

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,7 @@
 class Test {
     private static void main(){
-        System.out.println("Hello");
+        System.out.println("Hello01");
+ 	System.out.println("Hello02");
+	System.out.println("Hello03");
     }
 }
